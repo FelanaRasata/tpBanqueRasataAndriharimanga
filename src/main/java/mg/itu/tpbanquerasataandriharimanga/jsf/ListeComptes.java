f6d5aa07300baf58ef75199fd8a142c7150dbfae
FelanaRasata <77356909+FelanaRasata@users.noreply.github.com>
2024-04-12 12:03:47 +0300

    template & Page index.xhtml et test de l'application

---
+++ src/main/java/mg/itu/tpbanquerasataandriharimanga/jsf/ListeComptes.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSF/JSFManagedBean.java to edit this template
+ */
+package mg.itu.tpbanquerasataandriharimanga.jsf;
+
+import jakarta.inject.Named;
+import jakarta.faces.view.ViewScoped;
+import jakarta.inject.Inject;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import mg.itu.tpbanquerasataandriharimanga.entity.CompteBancaire;
+import mg.itu.tpbanquerasataandriharimanga.service.GestionnaireCompte.GestionnaireCompte;
+
+/**
+ *
+ * @author rasat
+ */
+@Named(value = "listeComptes")
+@ViewScoped
+public class ListeComptes implements Serializable {
+
+    List<CompteBancaire> allComptes;
+
+    @Inject
+    private GestionnaireCompte compteManager;
+
+    /**
+     * Creates a new instance of ListeComptes
+     */
+    public ListeComptes() {
+    }
+
+    public List<CompteBancaire> getAllComptes() {
+        if (allComptes == null) {
+            allComptes = compteManager.getAllComptes();
+        }
+        return allComptes;
+    }
+}
